07924c9527e117b3227aa5bd3bc8110a0a3b9ad0
gabriel <gb.ribeiropedroso@gmail.com>
2025-12-31 17:34:42 -0300

    collision_layer

--- grenade.gd
+++ grenade.gd
@@ -3,6 +3,14 @@ extends RigidBody2D
 @export var force := 550.0
 @export var angle := 45.0
 var launched := false
+var lifespan := 2.5
+var time_alive := 0.0
+
+func _process(delta: float) -> void:
+	time_alive += delta
+
+	if time_alive >= lifespan:
+		queue_free()
 
 func launch(direction: Vector2):
 	if launched:
