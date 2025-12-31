741fa093e66e81349d79a0c7214896e27342d43a
gabriel <gb.ribeiropedroso@gmail.com>
2025-12-31 16:08:49 -0300

    refat

--- barrel.gd
+++ barrel.gd
@@ -5,8 +5,8 @@ extends Node2D
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
-		hp -= 1
-		area.queue_free()
+		hp -= area.damage
+		area.get_parent().queue_free()
 
 		if hp <= 0:
 			queue_free() # aqui depois você pode trocar por explosão
